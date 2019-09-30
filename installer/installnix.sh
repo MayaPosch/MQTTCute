a29bded39083a8573f5f66058f0196dfa9dd60a5
Maya Posch <maya@nyanko.ws>
2019-09-30 17:33:52 +0200

    Update *NIX online installer.

--- installer/installnix.sh
+++ installer/installnix.sh
@@ -2,26 +2,42 @@
 
 #
 
-# If Debian-based, install the .deb file with the dependencies.
+# If Debian-based, use APT.
 if [ -x "$(command -v "apt")" ]; then 
 	sudo apt install git libpoco-dev qt5-default
 elif [ -x "$(command -v "apt-get")" ]; then 
 	sudo apt-get install git libpoco-dev qt5-default
 fi
 
-# If RPM-bsed, install the .rpm file with the dependencies.
+# If RPM/YUM-based, use its package manager
+if [ -x "$(command -v "yum")" ]; then 
+	sudo yum install qt5-qtbase-devel poco-devel
+fi
+
+# If RPM/DNF-based, use its package manager
+if [ -x "$(command -v "dnf")" ]; then 
+	sudo dnf install qt5-devel poco-devel
+fi
+
+# If Arch-based, use Pacman.
+if [ -x "$(command -v "pacman")" ]; then 
+	sudo pacman -S qt5-base poco
+fi
+
+# If MSYS2, use Pacman (no sudo).
 # if [ -x "$(command -v "rpm")" ]; then 
 	# sudo #
 # fi
 
 
 # Fetch the needed repositories.
-# TODO: check whether they exist already: if so, pull new commits instead.
+# TODO: check whether they exist already: if so, only pull new commits instead.
 git clone https://github.com/MayaPosch/ByteBauble.git
 git clone https://github.com/MayaPosch/NymphMQTT.git
 git clone https://github.com/MayaPosch/MQTTCute.git
 
 # Compile and install the dependencies.
+# TODO: If updating, clean and make lib if git pull found changes.
 make -C ByteBauble/src/cpp/ lib
 sudo cp ByteBauble/src/cpp/lib/libbytebauble.a /usr/local/lib/.
 sudo cp ByteBauble/src/cpp/src/*.h /usr/local/include/.
